b06fcbefcce6e473654a6d06939d79b4c05d16dc
phayeon <phayeon1222@gmail.com>
2022-10-06 17:56:36 +0900

    10-06 17:56

--- Greedy/for_change.py
+++ Greedy/for_change.py
@@ -1,13 +1,17 @@
 # 반복문(금액 입력)
 
 class Solution:
-    def solution(self):
-        
+    def solution(self,money):
+
         a = [50000,10000,5000,1000,500,100,50,10]
 
-        return 
+        for i in a:
+            count = money // i
+            money = money % i
+            print(f'{i}원 짜리 {count}개')
+        return
 
 if __name__ == '__main__':
     solution = Solution()
-
-    print(solution.solution())
+    money = input("교환 할 금액 입력 : ")
+    print(solution.solution(int(money)))
